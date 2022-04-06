6c1cc4eda57295e883022f47afdf5487c32a0c49
jamesizonfade <izonfadejames@gmail.com>
2022-04-06 11:51:39 +0100

    complete project

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-// fetch('https://api.adviceslip.com/advice')
-// .then(function(response) {
-//   const advice = response.json()
-//   console.log(advice)
+const adviceNumber = document.getElementById('advice-number')
 
-// })
-// .catch(function(error) {
-//   console.log('Looks like there was a problem: ', error);
-// });
+const advice = document.getElementById('advice')
 
-fetch("https://api.adviceslip.com/advice").then(function(response) {
+const generateAdvice = document.getElementById('generate-advice')
+
+
+const renderApp = () => {
+  fetch("https://api.adviceslip.com/advice").then(function(response) {
     return response.json();
   }).then(function(data) {
     console.log(data)
     console.log(data.slip.id);
+    adviceNumber.innerText = "ADVICE " + "#" + data.slip.id
     console.log(data.slip.advice);
+    advice.innerText = `"` + data.slip.advice + `"`
   }).catch(function() {
     console.log("Booo");
   });
+}
+
+renderApp()
+
+generateAdvice.addEventListener('click', renderApp)
